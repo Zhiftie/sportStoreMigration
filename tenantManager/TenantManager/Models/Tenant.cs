17596634f7e802cf6d12399265b8cc50d4f6ee96
Espen Nordli <espen.nordli@tietoevry.com>
2021-11-02 16:48:48 +0100

    Replacing tenant manager

---
+++ tenantManager/TenantManager/Models/Tenant.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TenantManager.Models
+{
+    public class Tenant
+    {
+        public String TenantName { get; set; }
+        [Key]
+        public int TenantId { get; set; }
+        public List<Customisation> Customisations { get; set; }
+    }
+}
