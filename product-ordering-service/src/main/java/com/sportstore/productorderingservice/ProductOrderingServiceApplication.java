64e3e1ded982b09d016c97a79222b27b1be165b9
Espen Nordli <espen.nordli@tietoevry.com>
2021-11-23 19:23:02 +0100

    Adding product-ordering-service and shopping-cart-service

---
+++ product-ordering-service/src/main/java/com/sportstore/productorderingservice/ProductOrderingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.sportstore.productorderingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductOrderingServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductOrderingServiceApplication.class, args);
+    }
+
+}
---
+++ product-ordering-service/src/test/java/com/sportstore/productorderingservice/ProductOrderingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sportstore.productorderingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductOrderingServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
