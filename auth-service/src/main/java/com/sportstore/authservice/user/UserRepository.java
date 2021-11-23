8463130d033130da54c3f44c00aa20b62e3a51d0
Espen Nordli <espen.nordli@tietoevry.com>
2021-11-23 19:27:47 +0100

    Removing auth-service

--- auth-service/src/main/java/com/sportstore/authservice/user/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.sportstore.authservice.user;
-
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-public interface UserRepository extends PagingAndSortingRepository<User, Long> {
-    User findByName(String name);
-}
--- auth-service/src/main/resources/db/migration/V3__INSERT_USER_data.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO public.user(
-    name, password, tenant)
-VALUES
-    ('TenantA', 'password', 'TenantA'),
-    ('TenantA1', 'password', 'TenantA'),
-    ('TenantB', 'password', 'TenantB'),
-    ('TenantB1', 'password', 'TenantB');
