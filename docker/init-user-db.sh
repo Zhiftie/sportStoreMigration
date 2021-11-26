4c66cf40eff21a82134602ba6b3ac9eedb34af5b
Espen Nordli <espen.nordli@tietoevry.com>
2021-11-26 10:39:15 +0100

    Add baseline for tenant-x-shipping-information-service

--- docker/init-user-db.sh
+++ docker/init-user-db.sh
@@ -8,6 +8,9 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
     CREATE DATABASE product_order;
     GRANT ALL PRIVILEGES ON DATABASE product_order TO dbuser;
 
-        CREATE DATABASE shopping_cart;
-        GRANT ALL PRIVILEGES ON DATABASE shopping_cart TO dbuser;
+    CREATE DATABASE shopping_cart;
+    GRANT ALL PRIVILEGES ON DATABASE shopping_cart TO dbuser;
+
+    CREATE DATABASE tenant_x_shipping_information
+    GRANT ALL PRIVILEGES ON DATABASE tenant_x_shipping_information TO dbuser;
 EOSQL
---
+++ tenant-x-shipping-information/src/main/java/com/sportstore/tenantxshippinginformation/TenantXShippingInformationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.sportstore.tenantxshippinginformation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TenantXShippingInformationServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TenantXShippingInformationServiceApplication.class, args);
+    }
+
+}
