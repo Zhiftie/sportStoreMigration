6b833f071118ac587be5e0e28e8bf0b15cce9396
Espen Nordli <espen.nordli@tietoevry.com>
2022-01-25 13:45:29 +0100

    Delete old code

--- productOrdering/server.js
+++
@@ -1,5 +0,0 @@
-import mongoose from 'mongoose';
-
-var rabbitConsumer = require('./consumers/productOrderConsumer');
-const db = mongoose.connect('mongodb://localhost:27017/orders', {useNewUrlParser: true});
-rabbitConsumer.openConnection(db);
--- shoppingCart/server.js
+++
@@ -1,5 +0,0 @@
-import app from './app';
-
-const port = process.env.PORT || '7003'; app.listen(port); 
-
-console.log(`Listening on port ${port}`);
